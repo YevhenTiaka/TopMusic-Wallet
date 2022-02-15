d60bfe8ed12467738e41ff2023ae13ee4c553163
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-15 13:19:08 +0200

    topmusic-wallet

--- Components/Modal.tsx
+++ Components/Modal.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/Modal.module.scss';
 const Modal = ({ setModalActive, children }: any) => {
   return (
     <section className={styles.modal} onClick={() => setModalActive(false)}>
-      <div className={styles.modal__container}>{children}</div>
+      <div className={styles.modal__container} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
     </section>
   );
 };
