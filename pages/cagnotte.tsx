fb5cf5d9563e64725aed1f348f91983d03019f80
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-16 11:46:10 +0200

    my cagnotte

--- pages/cagnotte.tsx
+++ pages/cagnotte.tsx
@@ -1,8 +1,21 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import styles from '../src/styles/Main.module.scss';
+import WalletManage from '../src/сomponents/walletManage';
+import WalletAmount from '../src/сomponents/walletAmount';
+import LastTips from '../src/сomponents/lastTips';
+import { FC } from 'react';
 
-const Cagnotte = () => {
-  return <></>;
+const Cagnotte: FC = () => {
+  return (
+    <main className={styles.main}>
+      <section className={styles.main__container}>
+        <WalletManage />
+        <section className={styles.main__right}>
+          <WalletAmount />
+          <LastTips />
+        </section>
+      </section>
+    </main>
+  );
 };
 
 export default Cagnotte;
