1289c1d9e5dda716cd8e2fa91cf65e61ae3510f3
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-16 11:45:55 +0200

    login page

--- pages/compte.tsx
+++ pages/compte.tsx
@@ -1,8 +1,35 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-
+import WalletAmount from '../src/сomponents/walletAmount';
+import styles from '../src/styles/Account.module.scss';
 const Compte = () => {
-  return <></>;
+  return (
+    <>
+      <section className={styles.account}>
+        <div className={styles.account__container}>
+          <div className={styles.account__container_block}>
+            <h1 className="text-3xl font-bold color:white underline">
+              Mes informations personnelles
+            </h1>
+            <div className={styles.account__container_inputs}>
+              <div>
+                <input type="text" name="" id="" />
+                <input type="text" name="" id="" />
+                <label htmlFor="">Homme</label>
+                <input type="radio" name="" id="" />
+                <label htmlFor="">Femme</label>
+                <input type="radio" name="" id="" />
+              </div>
+              <div>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+              </div>
+            </div>
+          </div>
+          <WalletAmount />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Compte;
