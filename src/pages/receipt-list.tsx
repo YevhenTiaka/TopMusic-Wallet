7d9998eaf418b7989081c81c1e4c185adcc8db2e
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-23 17:58:59 +0200

    refactoring

--- src/pages/receipt-list.tsx
+++ src/pages/receipt-list.tsx
@@ -1,18 +1,10 @@
 import { FC } from 'react';
-import { useRouter } from 'next/router';
+import Retour from 'components/retour';
 
 const Cagnotte: FC = () => {
-  const router = useRouter();
-
   return (
     <main className="receipt__main">
-      <div className="retour">
-        <i className="fa-solid fa-arrow-left">
-          <span onClick={() => router.back()} className="ml-4">
-            Retour
-          </span>
-        </i>
-      </div>
+      <Retour />
       <section className="receipt__list_section">
         <h1 className="pt-6 pl-6 font-bold text-xl mb-8">Historique des Tips</h1>
         <div className="receipt__list_block">
