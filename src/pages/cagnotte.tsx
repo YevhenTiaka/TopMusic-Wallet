d18d38d351702d3c547d716487c9f0bf22081b42
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-23 13:05:24 +0200

    cagnotte

--- src/pages/cagnotte.tsx
+++ src/pages/cagnotte.tsx
@@ -3,12 +3,10 @@ import WalletManage from 'components/walletManage';
 import WalletAmount from 'components/walletAmount';
 import LastTips from 'components/lastTips';
 import { FC } from 'react';
-import Notification from 'components/notification';
 
 const Cagnotte: FC = () => {
   return (
     <main className={styles.main}>
-      <Notification>Votre mot de passe a été modifié avec succès.</Notification>
       <section className={styles.main__container}>
         <WalletManage />
         <section className={styles.main__right}>
