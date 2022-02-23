612e44d5079f6020667b831b0db554f48cc4bbc1
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-23 13:03:23 +0200

    showing active links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,6 +10,10 @@ import topmusicLogo from 'styles/images/topmusic_logo.png';
 const Header: FC = () => {
   const router = useRouter();
   const [menu, setMenu] = useState<Boolean>(false);
+  const [active, setActive] = useState({
+    compte: false,
+    cagnotte: false
+  });
 
   return (
     <>
@@ -29,31 +33,29 @@ const Header: FC = () => {
                 : `${styles.header__nav}`
             }
           >
-            {router.pathname !== '/freemium' ? (
+            {router.pathname !== '/freemium' && (
               <li
                 className={
-                  router.pathname === '/cagnotte'
+                  active.cagnotte
                     ? `${styles.header__nav_active}`
                     : `${styles.header__nav_disabled}`
                 }
               >
                 <Link href="/cagnotte">
-                  <a>Ma cagnotte</a>
+                  <a onClick={() => setActive({ compte: false, cagnotte: true })}>Ma cagnotte</a>
                 </Link>
               </li>
-            ) : null}
+            )}
+
             <li
               className={
-                router.pathname === '/compte'
-                  ? `${styles.header__nav_active}`
-                  : `${styles.header__nav_disabled}`
+                active.compte ? `${styles.header__nav_active}` : `${styles.header__nav_disabled}`
               }
             >
               <Link href="/compte">
                 <a
-                  className={
-                    router.pathname === '/freemium' ? `${styles.header__nav_active}` : null
-                  }
+                  onClick={() => setActive({ compte: true, cagnotte: false })}
+                  className={router.pathname === '/freemium' && `${styles.header__nav_active}`}
                 >
                   Mon compte
                 </a>
