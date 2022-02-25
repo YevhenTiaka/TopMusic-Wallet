41c76612252ad5f4b11e62851e302053c665a2e0
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-25 12:26:57 +0200

    adding of notif need just logic of cond rendering

--- src/pages/cagnotte.tsx
+++ src/pages/cagnotte.tsx
@@ -3,14 +3,25 @@ import WalletManage from 'components/walletManage';
 import WalletAmount from 'components/walletAmount';
 import LastTips from 'components/lastTips';
 import { FC } from 'react';
-
+import UpdateNotification from 'components/updateNotification';
 const Cagnotte: FC = () => {
   return (
     <main className={styles.main}>
+      <UpdateNotification>
+        <div className="flex items-center">
+          <div className="notification__icon">X</div>
+
+          <div className="text-base ">
+            <strong>Votre moyen de paiement n'est pas à jour </strong>. <br /> Après le 18/12, votre
+            cagnotte sera désactivée et vous n'aurez plus accès aux fonctionnalités premium.
+          </div>
+          <div className="font-bold text-sm ml-6 mt-4">METTRE À JOUR MON MOYEN DE PAIEMENT</div>
+        </div>
+      </UpdateNotification>
       <section className={styles.main__container}>
         <WalletManage />
         <section className={styles.main__right}>
-          <WalletAmount />
+          <WalletAmount>Renouvellé le 14 octobre</WalletAmount>
           <LastTips />
         </section>
       </section>
