fd19e6f205596ca6097a536e17f5f807f7bd870b
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-25 14:48:14 +0200

    app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,16 @@ import { createContext, useState } from 'react';
 import Layout from 'components/layout';
 import 'styles/globals.css';
 
-export const Context = createContext<unknown>(defaultValue);
-
 function MyApp({ Component, pageProps }: any) {
   const [isShow, setIsShow] = useState<Boolean>(false);
 
   return (
     <>
-      <Context.Provider value={{ isShow, setIsShow }}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Context.Provider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
 
 export default MyApp;
-function defaultValue(defaultValue: any) {
-  throw new Error('Function not implemented.');
-}
