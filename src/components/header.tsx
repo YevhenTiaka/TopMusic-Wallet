1e6a2b678ca79ca267ad62b7a3428f8df8cd5262
TIAKA YEVHEN <gena43877@ukr.net>
2022-02-28 15:07:32 +0200

    added condition for showing header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,58 +17,80 @@ const Header: FC = () => {
 
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.header__container}>
-          <div className={styles.header__logo}>
-            <Link href="/">
-              <a>
-                <Image className={styles.header__logo_img} src={topmusicLogo} alt="Topmusic-logo" />
-              </a>
-            </Link>
-          </div>
-          <ul
-            className={
-              router.pathname === '/freemium'
-                ? `${styles.header__nav_freemium}`
-                : `${styles.header__nav}`
-            }
-          >
-            {router.pathname !== '/freemium' && (
-              <li
-                className={
-                  active.cagnotte
-                    ? `${styles.header__nav_active}`
-                    : `${styles.header__nav_disabled}`
-                }
-              >
-                <Link href="/cagnotte">
-                  <a onClick={() => setActive({ compte: false, cagnotte: true })}>Ma cagnotte</a>
-                </Link>
-              </li>
-            )}
-
-            <li
-              className={
-                active.compte ? `${styles.header__nav_active}` : `${styles.header__nav_disabled}`
-              }
-            >
-              <Link href="/compte">
-                <a
-                  onClick={() => setActive({ compte: true, cagnotte: false })}
-                  // className={router.pathname === '/freemium' && `${styles.header__nav_active}`}
-                >
-                  Mon compte
+      {router.pathname === '/login' ? (
+        <header>
+          <div className={styles.header__container}>
+            <div className={styles.header__logo}>
+              <Link href="/">
+                <a>
+                  <Image
+                    className={styles.header__logo_img}
+                    src={topmusicLogo}
+                    alt="Topmusic-logo"
+                  />
                 </a>
               </Link>
-            </li>
-          </ul>
-
-          <div className={styles.header__menu}>
-            <span className={styles.header__menu_user}>Benjamin N.</span>
-            <i className="fa-solid fa-angle-down" onClick={() => setMenu(!menu)} />
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
+      ) : (
+        <header className={styles.header}>
+          <div className={styles.header__container}>
+            <div className={styles.header__logo}>
+              <Link href="/">
+                <a>
+                  <Image
+                    className={styles.header__logo_img}
+                    src={topmusicLogo}
+                    alt="Topmusic-logo"
+                  />
+                </a>
+              </Link>
+            </div>
+            <ul
+              className={
+                router.pathname === '/freemium'
+                  ? `${styles.header__nav_freemium}`
+                  : `${styles.header__nav}`
+              }
+            >
+              {router.pathname !== '/freemium' && (
+                <li
+                  className={
+                    active.cagnotte
+                      ? `${styles.header__nav_active}`
+                      : `${styles.header__nav_disabled}`
+                  }
+                >
+                  <Link href="/cagnotte">
+                    <a onClick={() => setActive({ compte: false, cagnotte: true })}>Ma cagnotte</a>
+                  </Link>
+                </li>
+              )}
+
+              <li
+                className={
+                  active.compte ? `${styles.header__nav_active}` : `${styles.header__nav_disabled}`
+                }
+              >
+                <Link href="/compte">
+                  <a
+                    onClick={() => setActive({ compte: true, cagnotte: false })}
+                    // className={router.pathname === '/freemium' && `${styles.header__nav_active}`}
+                  >
+                    Mon compte
+                  </a>
+                </Link>
+              </li>
+            </ul>
+
+            <div className={styles.header__menu}>
+              <span className={styles.header__menu_user}>Benjamin N.</span>
+              <i className="fa-solid fa-angle-down" onClick={() => setMenu(!menu)} />
+            </div>
+          </div>
+        </header>
+      )}
       {menu && <UserMenu />}
     </>
   );
