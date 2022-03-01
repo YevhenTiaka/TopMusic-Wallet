f70cc3b9e0a8681b685b238d5e436861b0765ea2
TIAKA YEVHEN <gena43877@ukr.net>
2022-03-01 14:46:19 +0200

    open cagnotte for freemium page

--- src/components/openCagnotte.tsx
+++ src/components/openCagnotte.tsx
@@ -2,12 +2,51 @@ import { FC } from 'react';
 
 const OpenCagnotte: FC = () => {
   return (
-    <>
-      <section className="section__cangotte">
-        <div className=" bg-black w-40 h-40"></div>
-        <div className=" bg-black w-40 h-40"></div>
-      </section>
-    </>
+    <div className="open__cagnotte">
+      <div className="open__cagnotte_block">
+        <h1 className="text-2xl font-bold mb-6">Pourquoi ouvrir une cagnotte ?</h1>
+        <div className="mb-4">
+          <strong>
+            {' '}
+            Soutenir les artistes que vous voulez, quand vous voulez. <br />{' '}
+          </strong>{' '}
+          Si l'application reste gratuite et libre d'accès, <br /> vous avez l'opportunité de
+          soutenir les artistes financièrement grâce à la création de cagnotte
+        </div>
+        <div className="mt-4 mb-4 font-bold">
+          Profitez de l'ensemble des fonctionnalités <br /> premium :
+        </div>
+        <div className="flex mb-4">
+          <div className="cagnotte__img"></div>
+          <div>Écoute illimitée</div>
+        </div>
+        <div className="flex mb-4">
+          <div className="cagnotte__img"></div>
+          <div>Création de playlists</div>
+        </div>
+        <div className="flex mb-4">
+          <div className="cagnotte__img"></div>
+          <div>Qualité audio supérieure</div>
+        </div>
+        <div className="flex mb-8">
+          <div className="cagnotte__img"></div>
+          <div>Mode hors connexion</div>
+        </div>
+      </div>
+      <div className="open__cagnotte_block">
+        <h1 className="text-2xl font-bold mb-6">Comment ça fonctionne ?</h1>
+        <div className="mb-6">
+          <div className="mb-6 ">
+            Vous souscrivez à un abonnement mensuel du montant de votre choix. <strong></strong>{' '}
+            C'est le montant de votre cagnotte. (Minimum 5€).
+          </div>
+          <br /> La <strong>fonction Tip </strong> vous permet de distribuer cette cagnotte aux
+          artistes comme vous le souhaitez tout au long du mois.
+        </div>
+
+        <a className="learn__more">EN SAVOIR PLUS</a>
+      </div>
+    </div>
   );
 };
 
