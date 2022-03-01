7faecfe982fa30e28b2ede6847ecbf77bbdeac94
TIAKA YEVHEN <gena43877@ukr.net>
2022-03-01 14:46:35 +0200

    login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,24 +1,47 @@
 const Login = () => {
   return (
     <>
+      <div className="left__bg_one"></div>
       <main className="h-screen">
         <section className="m-auto">
-          <h1>Connexion</h1>
-          <div className="flex">
-            <div>
-              <label htmlFor=""></label>
-              <input type="text" />
-              <label htmlFor=""></label>
-              <input type="text" />
-              <span>Mot de passe oublié ?</span>
-              <button></button>
-              <span>Première fois sur TopMusic ? Inscrivez-vous ?</span>
+          <h1 className="text-center font-extrabold text-5xl mb-20">Connexion</h1>
+          <div className="flex justify-center">
+            <div className="flex flex-col mr-10">
+              <label className="text-xs opacity-70 font-bold" htmlFor="">
+                ADRESSE EMAIL
+              </label>
+              <input className="outline-none h-12 border-b-2 mb-4 indent-2" type="text" />
+              <label className="text-xs opacity-70 font-bold" htmlFor="">
+                MOT DE PASSE
+              </label>
+
+              <input className="outline-none h-12 border-b-2 indent-2" type="password" />
+              <span className="forgot__password">Mot de passe oublié ?</span>
+              <a className="connecter__btn">SE CONNECTER</a>
+              <a className="register__btn">
+                Première fois sur TopMusic ? <u> Inscrivez-vous ? </u>
+              </a>
             </div>
-            <div>
-              <div>
-                <a href="facebook.com"></a>
+            <div className="ml-10">
+              <div className="mb-4 mt-6">
+                <a className="social__link" href="facebook.com">
+                  <i className="fa-brands fa-facebook"></i>
+                  Continuer avec Facebook
+                </a>
               </div>
-              <span>Login par TopChrétien</span>
+              <div className="mb-4 mt-6">
+                <a className="social__link" href="facebook.com">
+                  <i className="fab fa-google fa-10x"></i>
+                  Continuer avec Google
+                </a>
+              </div>
+              <div className="mb-4 mt-6">
+                <a className="social__link" href="facebook.com">
+                  <i className="fa-brands fa-facebook"></i>
+                  Continuer avec Facebook
+                </a>
+              </div>
+              <a className="topchretien__login">Login par TopChrétien</a>
             </div>
           </div>
         </section>
