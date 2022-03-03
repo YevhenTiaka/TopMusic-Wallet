4a98877d5e79961c3497da55c0f8671f58b7a600
TIAKA YEVHEN <gena43877@ukr.net>
2022-03-03 13:10:31 +0200

    refactoring

--- src/pages/compte.tsx
+++ src/pages/compte.tsx
@@ -10,104 +10,162 @@ import ModifierPasse from 'components/modifierPasse';
 const Compte: FC = () => {
   const [isAuth, setIsAuth] = useState(true);
   const [value, setValue] = useState<number>(100);
+  const [disactive, setDisactive] = useState(true);
+
+  const [show, setShow] = useState(false);
+
+  const handleUpdated = () => {
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+  };
 
   return (
     <>
-      {isAuth ? (
-        <section className="mt-20 h-max flex justify-center  mr-5 ml-5 ">
-          <div className="flex flex-col  mr-8 ">
-            <Abonement>
-              <div className="flex">
+      {disactive ? (
+        <div className="h-max mt-10 flex mr-12 ml-12">
+          <div className="mr-6 w-3/4">
+            <section className="abonement__disactive">
+              <h1 className="text-xl font-bold mb-4">Mon abonnement</h1>
+              <div className="flex items-center">
                 <div className="">
-                  <div className="mb-4 text-base">
-                    Vous avez un <strong>Compte Gratuit sans cagnotte.</strong>
-                  </div>
-                  <div className=" text-xs">
-                    Vous profitez de l'ensemble du catalogue mais n'avez pas accès à<br /> toutes
-                    les fonctionnalités et vous ne pouvez pas soutenir les artistes.
+                  <div className="text-base w-11/12   ">
+                    Vous avez récemment résilié votre abonnement. <br />
+                    <strong>Vous disposez de l’abonnement premium jusqu’au 14/10</strong>,date après
+                    laquelle vous basculerez sur <strong> un compte sans cagnotte.</strong>
+                    <br />
+                    <br />
+                    Pour profiter de toutes les fonctionnalités et soutenir les artistes, vous
+                    pouvez
+                    <strong> réactiver votre cagnotte.</strong>
                   </div>
                 </div>
-                <button className="button__cagnotte">OUVRIR UNE CAGNOTTE</button>
+
+                <Link href="/cagnotte">
+                  <a className="button__reactiver">RÉACTIVER MA CAGNOTTE</a>
+                </Link>
               </div>
-            </Abonement>
-            <OpenCagnotte />
+            </section>
             <Personal />
             <div className="mt-10 mb-20">
               <ModifierPasse />
             </div>
           </div>
+
           <div>
-            <WalletAmount>
-              <div className="no__cagnotte">Vous n'avez pas de cagnotte</div>
+            <WalletAmount handleUpdated={handleUpdated}>
+              {' '}
+              <div className="flex justify-between">
+                <span className="amount__text">Vous possédez dans votre cagnotte</span>
+                <div>
+                  <span className="right_block_num">13</span>
+                  <span className="right_block_tc">Tc</span>
+                </div>
+              </div>
             </WalletAmount>
-            <main className="h-screen">
-              <div className="main__block_freemium">
-                <h1 className="font-bold text-3xl mb-4">Je crée ma cagnotte</h1>
-                <span className="text-base">
-                  Sur TopMusic, nous utilisons le TopCoin. La formule est simple :
-                </span>
-
-                <div className="recharge">
-                  Pour {value / 10}€ vous rechargez votre cagnotte de {value} Tc.
-                </div>
-                <div className="flex justify-center items-center mt-4">
-                  <button
-                    disabled={value <= 0}
-                    onClick={() => setValue(value - 5)}
-                    className="circle__btn"
-                  >
-                    -
-                  </button>
-                  <div className="circle__btn_value">{value} Tc/mois</div>
-                  <button onClick={() => setValue(value + 10)} className="circle__btn">
-                    +
-                  </button>
-                </div>
-                <span className="flex justify-center mb-6 mt-4 decoration-gray-50 opacity-50 text-base">
-                  Soit {value / 10}€/mois
-                </span>
-                <div className="flex flex-col items-center">
-                  <button className="create__cagnotte">JE CRÉE MA CAGNOTTE</button>
-                </div>
-              </div>
-            </main>
+            <div className="votre__text">Votre abonnement s’arrêtera le 14/10</div>
           </div>
-        </section>
+        </div>
       ) : (
-        <section className="flex flex-row  mt-20 mr-10 ml-10 justify-center ">
-          <div className="mr-10">
-            <Personal />
-            <Modifier />
-            <Abonement>
-              <div className="flex flex-col">
-                <div className="">
-                  <div className="text-base">
-                    Vous avez un <strong>Compte Gratuit sans cagnotte.</strong>
+        <>
+          {isAuth ? (
+            <section className="mt-20 h-max flex justify-center  mr-5 ml-5 ">
+              <div className="flex flex-col  mr-8 ">
+                <Abonement>
+                  <div className="flex">
+                    <div className="">
+                      <div className="mb-4 text-base">
+                        Vous avez un <strong>Compte Gratuit sans cagnotte.</strong>
+                      </div>
+                      <div className=" text-xs">
+                        Vous profitez de l'ensemble du catalogue mais n'avez pas accès à<br />{' '}
+                        toutes les fonctionnalités et vous ne pouvez pas soutenir les artistes.
+                      </div>
+                    </div>
+                    <button className="button__cagnotte">OUVRIR UNE CAGNOTTE</button>
                   </div>
-                  <div className="text-base">
-                    En plus de profiter de toutes les fonctionnalités premium, vous soutenez les
-                    artistes par vos tips.
+                </Abonement>
+                <OpenCagnotte />
+                <Personal />
+                <div className="mt-10 mb-20">
+                  <ModifierPasse />
+                </div>
+              </div>
+              <div>
+                <WalletAmount>
+                  <div className="no__cagnotte">Vous n'avez pas de cagnotte</div>
+                </WalletAmount>
+                <main className="h-screen">
+                  <div className="main__block_freemium">
+                    <h1 className="font-bold text-3xl mb-4">Je crée ma cagnotte</h1>
+                    <span className="text-base">
+                      Sur TopMusic, nous utilisons le TopCoin. La formule est simple :
+                    </span>
+
+                    <div className="recharge">
+                      Pour {value / 10}€ vous rechargez votre cagnotte de {value} Tc.
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                      <button
+                        disabled={value <= 0}
+                        onClick={() => setValue(value - 5)}
+                        className="circle__btn"
+                      >
+                        -
+                      </button>
+                      <div className="circle__btn_value">{value} Tc/mois</div>
+                      <button onClick={() => setValue(value + 10)} className="circle__btn">
+                        +
+                      </button>
+                    </div>
+                    <span className="flex justify-center mb-6 mt-4 decoration-gray-50 opacity-50 text-base">
+                      Soit {value / 10}€/mois
+                    </span>
+                    <div className="flex flex-col items-center">
+                      <button className="create__cagnotte">JE CRÉE MA CAGNOTTE</button>
+                    </div>
+                  </div>
+                </main>
+              </div>
+            </section>
+          ) : (
+            <section className="flex flex-row  mt-20 mr-10 ml-10 justify-center ">
+              <div className="mr-10">
+                <Personal />
+                <Modifier />
+                <Abonement>
+                  <div className="flex flex-col">
+                    <div className="">
+                      <div className="text-base">
+                        Vous avez un <strong>Compte Gratuit sans cagnotte.</strong>
+                      </div>
+                      <div className="text-base">
+                        En plus de profiter de toutes les fonctionnalités premium, vous soutenez les
+                        artistes par vos tips.
+                      </div>
+                    </div>
+
+                    <Link href="/montant-mise-a-jour">
+                      <a className="button__modifier">MODIFIER MON ABONNEMENT</a>
+                    </Link>
+                  </div>
+                </Abonement>
+              </div>
+              <WalletAmount>
+                {' '}
+                <div className="flex justify-between">
+                  <span className="amount__text">Vous possédez dans votre cagnotte</span>
+                  <div>
+                    <span className="right_block_num">13</span>
+                    <span className="right_block_tc">Tc</span>
                   </div>
                 </div>
-
-                <Link href="/montant-mise-a-jour">
-                  <a className="button__modifier">MODIFIER MON ABONNEMENT</a>
-                </Link>
-              </div>
-            </Abonement>
-          </div>
-          <WalletAmount>
-            {' '}
-            <div className="flex justify-between">
-              <span className="amount__text">Vous possédez dans votre cagnotte</span>
-              <div>
-                <span className="right_block_num">13</span>
-                <span className="right_block_tc">Tc</span>
-              </div>
-            </div>
-            <span className="right_block_text_b">Renouvellé le 14 octobre</span>
-          </WalletAmount>
-        </section>
+                <span className="right_block_text_b">Renouvellé le 14 octobre</span>
+              </WalletAmount>
+            </section>
+          )}
+        </>
       )}
     </>
   );
